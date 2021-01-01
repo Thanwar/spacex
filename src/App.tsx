c0abdb42724b3cea6566ff3894755d011be98064
Thanwar <thawardas25@gmail.com>
2021-01-01 22:29:33 +0500

    completed the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import LaunchList from './components/LaunchList';
+import LaunchProfile from './components/LaunchProfile';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const [id, setId] = React.useState(42);
+  const handleIdChange = React.useCallback(newId => {
+    setId(newId);
+  }, []);
+
   return (
     <div className="App">
-      <h1>Space X</h1>
+      <LaunchList handleIdChange={handleIdChange} />
+      <LaunchProfile id={id} />
     </div>
   );
-}
+};
 
 export default App;
